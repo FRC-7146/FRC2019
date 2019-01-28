bbf1a7c55c5ea78a685771e893dbbbe59d2c7141
Unknown <1198995013@qq.com>
2019-01-28 12:47:55 +0800

    Train barely working Cascade Classifier
    Use with scale 5, 180 min neibhors

--- Cascade Classifiers/Hatch Pannel Training/sample_gen.sh
+++ Cascade Classifiers/Hatch Pannel Training/sample_gen.sh
@@ -1,3 +1,4 @@
 #!/bin/bash 
 
-opencv_createsamples -img ./positive_images/* -maxzangle 6.28 -maxyangle 0.9 -maxxangle 0.9 -w 50 -h 50 -vec pos-samples.vec
+#opencv_createsamples -img ./positive_images/* -maxzangle 6.28 -maxyangle 1.0 -maxxangle 1.0 -w 80 -h 80 -vec pos-samples.vec -num 1000
+opencv_createsamples -img ./positive_images/* -maxzangle 2.0 -maxyangle 0.2 -maxxangle 0.2 -w 150 -h 150 -vec pos-samples.vec -num 4000
--- Cascade Classifiers/Hatch Pannel Training/view_positive.sh
+++ Cascade Classifiers/Hatch Pannel Training/view_positive.sh
@@ -1,3 +1,3 @@
  #!/bin/bash 
 
-opencv_createsamples -vec pos-samples.vec  -w 50 -h 50
+opencv_createsamples -vec pos-samples.vec  -w 150 -h 150
